436561be0f270e4e268280dd434dd311cb736c55
FedeSol89 <federico.solini.89@gmail.com>
2025-07-08 17:47:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import os
+import requests
+import feedparser
+import datetime
+import io
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+from pytz import timezone
+from telegram import ParseMode
+from telegram.ext import
 add main bot file
